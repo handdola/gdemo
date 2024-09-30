c35048f35d218ccd83509c531d1e3124be854cce
“handdola” <handdola@gmail.com>
2024-09-30 14:53:08 +0900

    Initial deployment of Flutter Web App

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "534948205"
+    serviceWorkerVersion: "4188088791"
   }
 });
